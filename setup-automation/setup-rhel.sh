7bf728fc1be0793548479cbed13a19cbb6656a37
Matt Micene (RHT) <mmicene@redhat.com>
2025-09-16 15:42:34 -0400

    Initial config update to 10

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-USER=rhel
 
-dnf install -y tmux
-
-echo "Adding wheel" > /root/post-run.log
-usermod -aG wheel rhel
-
-echo "Fixing permissions" >> /root/post-run.log
-chown -R rhel:rhel /home/rhel/
+# Removed unnecessary tasks, this is a no-op
+echo "DONE" > /root/post-run.log
