0b62b7458aeb74c907b4386922e38480865115bd
Matt Micene (RHT) <mmicene@redhat.com>
2025-09-16 17:20:11 -0400

    Move package installs to setup, add rule for ansible

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-# Removed unnecessary tasks, this is a no-op
+# Pull cowsay from a release tarball in the repo
+curl -L -O https://github.com/Code-Hex/Neo-cowsay/releases/download/v2.0.4/cowsay_2.0.4_Linux_x86_64.tar.gz
+
+# Allow ansible to run runtime-automation 
+echo "allow perm=any auid=1000 trust=1 : dir=/home/rhel/.ansible/" > /etc/fapolicyd/rules.d/50-ansible.rules
+
 echo "DONE" > /root/post-run.log
+
